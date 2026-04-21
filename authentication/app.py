7ede95c49ac5d6bcd425b56dd3527abccef7cbf0
vishuvishwas005-cmyk <vishuvishwas005@gmail.com>
2026-04-21 09:53:20 +0530

    Restore authentication app files

---
+++ authentication/app.py
@@ -0,0 +1,49 @@
+﻿import os
+from flask import Flask
+from flask_login import LoginManager
+from dotenv import load_dotenv
+from models import db, User
+from routes import mail
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Configuration
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-12345')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Flask-Mail configuration
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+
+    # Initialize extensions
+    db.init_app(app)
+    mail.init_app(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Register blueprints
+    from routes import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
